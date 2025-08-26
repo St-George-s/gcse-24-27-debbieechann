2a39a8547155395f14ba5f755d6fa58c070b234f
debbieechann <cha054@stge.org.uk>
2025-08-26 09:24:41 +0000

    day 1 madlibs

---
+++ madlibs.py
@@ -0,0 +1,7 @@
+name= input("enter a name")
+print ("oh woww so ur " + name)
+age= input("okayy thats cool!! now enter an age")
+food= input("enter your favourite food")
+songs= input("enter your favourite song")
+
+
