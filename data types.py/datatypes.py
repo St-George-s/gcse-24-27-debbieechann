94a4cd744ae250646bb5f8777d3946f1ea574bb1
debbieechann <cha054@stge.org.uk>
2025-09-02 09:24:19 +0000

    DATA types

---
+++ data types.py/datatypes.py
@@ -0,0 +1,40 @@
+ # DataTypes
+name = "debbie" # this is a string
+myage = "14" # this is an integer
+height = "162.5" # this is a decimal number (float/real)
+nonchalant = "yes" #this is a boolean (true/false)
+
+#casting (change from 1 data type to another)
+print  (myage) #connecting 2 strings tgt
+age = input ("enter age")
+print (age + "10") #add 2 integers together
+AgeAsAnInt = int(age)
+print ("soo ur age is" + str(AgeAsAnInt))
+
+#data types - more examples
+stillAnInteger = -4
+mynumber= "91845152" #always store as a string
+
+#arthmetic operators
+add = 10 + 10 
+subtract = 10-10
+division= 10/10 #will output a float
+multiplication = 10*10
+integerDivision = 11/10 #forces output to be integer
+print(add, subtract, division, multiplication)
+print(integerDivision)
+modulo= 2000 % 501 #remainder 0f division
+print (modulo)
+exponent= 2 ** 4 #to the power of 
+print (exponent)
+
+#activity 1= take 2 inputs, multiply them together and output answer
+firstnumber= input ("type a number between 1-100)")
+secondnumber= input ("type another number between 1-100")
+multiplication (int(firstnumber)*(secondnumber) )
+
+
+#activity 2= input user age, input age times 7
+
+
+#activity 3= take radius as input, output volume of sphere (y= 4/3 x pi x r^3)
---
+++ songmadlibs.py
@@ -0,0 +1,10 @@
+animal = input("enter person's name")
+
+
+print ("Can't remember how to say "+ animal +"'s name")
+print ("let alone count all the freckles on "+animal+ "'s face")
+ 
+place = input ("a sentimental place/object??")
+print ("A distant "+place+ " I used to know")
+print ("oh i guess that's just the way things go")
+
