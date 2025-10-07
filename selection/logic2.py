14c442da459747cb5a09e3cce04e20495fa83965
debbieechann <cha054@stge.org.uk>
2025-10-07 22:02:14 +0000

    gijwdogj

--- selection/logic2.py
+++ selection/logic2.py
@@ -1,6 +1,48 @@
 # Question 1
-temperature = (int)(input("Enter the temperature: ")    )
-is_sunny = (int)(input ("is it sunny"))
-if is_sunny == ("yes"):
-    result = temperature > 15 and is_sunny
-print("Q1:", result)
+temperature = 18
+is_sunny = True
+print("Q1:", temperature > 15 and is_sunny)
+
+# Question 2
+temperature = 12
+is_raining = True
+print("Q2:", temperature > 15 or is_raining)
+
+# Question 3
+num_apples = 8
+print("Q3:", not (num_apples > 10))
+
+# Question 4
+age = 20
+has_licence = True
+print("Q4:", age > 18 and has_licence)
+
+# Question 5
+speed = 55
+is_raining = False
+print("Q5:", speed > 60 or not is_raining)
+
+# Question 6
+hours_studied = 4
+feels_prepared = True
+print("Q6:", (hours_studied > 5 or feels_prepared))
+
+# Question 7
+assignments_completed = 6
+assignments_pending = 1
+print("Q7:", assignments_completed > 5 and assignments_pending < 2)
+
+# Question 8
+savings = 100
+item_price = 120
+on_sale = True
+print("Q8:", savings > item_price or on_sale)
+
+# Question 9
+is_sunny = False
+is_weekend = False
+is_weekday = not is_weekend
+print("Q9:", (is_sunny and is_weekend) or (not is_sunny and is_weekday))
+
+
+
