f0d1bd56026694fa83eac14968adffabf62dac80
debbieechann <cha054@stge.org.uk>
2025-10-07 21:55:41 +0000

    hw

---
+++ data types/arrays.py
@@ -0,0 +1,18 @@
+heights = [1.75, 1.80,  1.55]
+names = ["dave", "Bob", "simon"]
+
+print(names [1] )  #prints dave
+print(heights [1]) #prints 3.8
+
+#loops over names arrays and print
+for counter in range(len(names)) :
+    print(names[counter]) 
+    print(heights[counter])
+
+#append add to array
+heights.append(2.2)
+names.append("jimmy")
+
+print (heights)
+print (names)
+
---
+++ data types/arrays2.py
@@ -0,0 +1,31 @@
+# cities = ["london", "edinburgh", "cardiff", "belfast", "glasgow"]
+# population = [8908, 482, 366, 341, 635]
+
+# for counter in range (len(cities)):
+#     print (cities[counter] + " has a population of " + str(population[counter]))
+           
+#linearsearch
+# usercity = input("what city's population would u like ? :")
+# found = False
+# for counter in range (len(cities)):
+#     if usercity == cities[counter]:
+#         print("the population of " + usercity + " is " + str(population[counter]))
+#         found = True
+# if not found: print("sorry we dont have that city")
+
+#Q1
+# integers = [1, 2, 3, 4, 5]
+# print(integers)
+#Q2
+integer2 = [10, 20, 30, 40, 50]
+print(integer2[0])
+print(integer2[4])
+
+#Q3
+integer3= [10, 20, 30, 40, 50]
+integer3[1] = 100
+print(integer3)
+
+#q4
+integers = [5, 10, 15, 20, 25]
+print(integers)
